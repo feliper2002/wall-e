56da75614771ef953ad0bf8b9a78415a35fdd60a
Felipe Ribeiro <feliper.dev@gmail.com>
2022-01-10 20:12:24 -0300

    wall-e

--- lib/walle/walle.dart
+++ lib/walle/walle.dart
@@ -63,7 +63,7 @@ class WallePaint extends CustomPainter {
   void paint(Canvas canvas, Size size) {
     //////////////////// [EYE] ////////////////////
 
-    // final eyeDimensions = Offset(size.width * .28, size.height * .12);
+    final eyeLeftPos = Offset(size.width * .378, size.height * .4);
 
     final eyeBasePaint = Paint()
           ..color = AppColors.eyeBase
@@ -73,20 +73,18 @@ class WallePaint extends CustomPainter {
 
     Path eyeLeftPath = Path()
           ..moveTo(size.width * .42, size.height * .326)
-          ..quadraticBezierTo(size.width * .43, size.height * .32,
-              size.width * .41, size.height * .326)
-          ..quadraticBezierTo(size.width * .36, size.height * .3478,
-              size.width * .28, size.height * .371)
-          ..quadraticBezierTo(size.width * .224, size.height * .4,
-              size.width * .224, size.height * .43)
-          ..quadraticBezierTo(size.width * .282, size.height * .49,
-              size.width * .354, size.height * .485)
-          ..quadraticBezierTo(size.width * .421, size.height * .464,
-              size.width * .456, size.height * .419)
-          ..quadraticBezierTo(size.width * .421, size.height * .325,
-              size.width * .41, size.height * .326)
+          ..quadraticBezierTo(size.width * .354, size.height * .35,
+              size.width * .266, size.height * .376)
+          ..quadraticBezierTo(size.width * .2, size.height * .472,
+              size.width * .285, size.height * .481)
+          ..quadraticBezierTo(size.width * .328, size.height * .494,
+              size.width * .397, size.height * .4737)
+          ..quadraticBezierTo(size.width * .47, size.height * .418,
+              size.width * .424, size.height * .328)
         //
         ;
+
+    eyeLeftPath = eyeLeftPath.transform(rotatePath(eyeLeftPos, -30));
 
     drawPathWithStroke(canvas, eyeLeftPath, eyeBasePaint);
     //////////////////// [EYE] ////////////////////
