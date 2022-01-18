90cc91f861dc41508683fda21b53b6c7bb793578
Felipe Ribeiro <feliper.dev@gmail.com>
2022-01-18 11:05:57 -0300

    FIx imports

--- lib/walle/parts/eyes.dart
+++ lib/walle/parts/eyes.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:walle_fteam/utils/colors.dart';
 import 'package:walle_fteam/walle/parts/pupils.dart';
